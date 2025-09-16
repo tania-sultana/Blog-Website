ecebf1a8adfe27bbdaa8145a37bd1c386346dd77
= <=>
2025-09-16 15:47:17 +0600

    complete design

--- scripts.js
+++ scripts.js
@@ -10,3 +10,21 @@ menuIcon.addEventListener("click", () => {
     }
 
 });
+
+
+
+// FAQ PORTION START HERE
+var action="click";
+var speed="500";
+
+$(document).ready(function() {
+    // Question handler
+    $('li.q').on(action, function() {
+        // Get next element
+        $(this).next()
+            .slideToggle(speed)
+        // Select all other answers
+                .siblings('li.a')
+                    .slideUp();
+    });
+});
